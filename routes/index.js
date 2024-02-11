ab7cb30cf9eec941d50171417c453ce497263e32
Sayaka200532267 <200532267@student.georgianc.on.ca>
2024-02-11 15:32:09 -0500

    this is my second commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'My portfolio', 
   background: 'I am a student of Georgian College, and I\'m taking the course of Web Development. I am from Japan, (Not From China!!) and I had worked as an administrative assistant. However, I want to explore the world and learn English and web development, so I came to Canada to seek out the adventure.', 
-  backgroundImg: 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  backgroundImg: 'images/home.png',
   Interests: 'My interests in the coding fields are making webpages using React and Node.js', Goals: 'My goal is to become a front end developer. Also, I would like to know about AI in the future,  since AI is evolving and I think it will be a big industry, and might replace to the developers.'});
 });
 
@@ -29,13 +29,44 @@ router.get('/contact', function(req, res, next){
   backgroundImg: 'https://images.unsplash.com/photo-1423666639041-f56000c27a9a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   contact: 'Please contact me at: (705)716-3492 or email me at: sayakaohara@outlook.com'})
 })
-
 router.get('/resume', function(req, res, next){
-  res.render('Resume', {title: 'Resume', Education1: 'Georgian College, Barrie, ON, Interactive Media Design - Web, 2022-2024',
-  Education2: 'Tokyo Woman\'\s Christian University, Tokyo, Japan, Bachelor of Arts in Linguistic, 2012-2016',
-  Experience1: 'Web Development Assistant, Toronto, ON, 2023, I was responsible for editing webpages using Drupal.  and JavaScript. I also used JavaScript and API to create a responsive webpage.',
-  Experience2: 'Administrative Assistant, Tokyo, Japan, 2020-2022, I was responsible for ordering the stock, with adjusting the stock. I also acted as a collaborator with the other teams.',
-  Experience3: 'Administrative Assistant, Tokyo, Japan, 2016-2019, I was responsible for creating the documents such as estimate, invoice, and purchase order. I also sold products to the customers.',  backgroundImg: 'https://images.unsplash.com/photo-1602407294553-6ac9170b3ed0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'})
-
-})
+  res.render('Resume', {
+    title: 'Resume',
+    content: {
+      education: [
+        {
+          school: 'Georgian College',
+          location: 'Barrie, ON',
+          major: 'Interactive Media Design - Web',
+          year: '2022-2024'
+        },
+        {
+          school: 'Tokyo Woman\'s Christian University',
+          location: 'Tokyo, Japan',
+          major: 'Bachelor of Arts in Linguistic',
+          year: '2012-2016'
+        }
+      ],
+      experiences: [
+        {
+          title: 'Web Development Assistant',
+          location: 'Toronto, ON',
+          year: '2023',
+        },
+        {
+          title: 'Administrative Assistant',
+          location: 'Tokyo, Japan',
+          year: '2020-2022',
+           },
+        {
+          title: 'Administrative Assistant',
+          location: 'Tokyo, Japan',
+          year: '2016-2019',
+         }
+      ],
+      backgroundImg: 'images/resume.png',
+      resume: 'https://drive.google.com/file/d/1hNQZ3X0W1Rm1Z0Tb5a1L6d2u6XJv5Q8j/view?usp=sharing'
+    }
+  });
+});
 module.exports = router;
