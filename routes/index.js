c0367dd9bd6a4022189862a27c1ac5620a2d601b
Sayaka200532267 <124095624+Sayaka200532267@users.noreply.github.com>
2024-02-11 18:08:46 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/about', function(req, res, next) {
 router.get('/skills', function(req, res, next){
   res.render('skills', 
   {backgroundImg: 'https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'skills',
+    title: 'Skills',
     skills: [
       { type: 'Programming Languages', lang: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'JSON', 'SQL', 'SASS', 'LESS', 'jQuery', '.Net', 'C#', 'PHP'] },
       { type: 'Additional Skills', lang: ['Express', 'RESTful API', 'MongoDB', 'Git', 'GitHub', 'Bootstrap', 'VSCode', 'Adobe Photoshop', 'Adobe Illustrator', 'Jira'] },
