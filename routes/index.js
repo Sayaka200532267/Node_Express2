edb445fb5b77abf4026291b8ff1b64549f84d9b2
Sayaka200532267 <124095624+Sayaka200532267@users.noreply.github.com>
2024-02-11 18:06:45 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/about', function(req, res, next) {
 });
 })
 router.get('/skills', function(req, res, next){
-  res.render('Skills', 
+  res.render('skills', 
   {backgroundImg: 'https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'skills',
     skills: [
